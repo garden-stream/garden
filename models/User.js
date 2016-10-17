a14435ee53b87a547ab72f0c668c2166cdf9a63c
Nathan Bland <nathan.bland@gmail.com>
2016-10-17 10:58:40 -0600

    Adding proper fields to user model for following, and followers. Added routes to get following and followers in /api/user

--- models/User.js
+++ models/User.js
@@ -1,12 +1,27 @@
 let mongoose = require('mongoose');
 
+let postSchema = mongoose.Schema({
+  contentType: String,
+  content: String
+})
+
 let userSchema = mongoose.Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true },
-  email: String
-});
+  following: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    index: true,
+  }],
+  followers: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    index: true,
+  }],
+  posts: [postSchema]
+})
 
-let Users = mongoose.model('Users', userSchema);
+let Users = mongoose.model('user', userSchema);
 
 // make this available to our users in our Node applications
 module.exports = Users;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 let bcrypt = require('bcrypt')
-let User = require('../models/User');
+let User = require('../models/User')
 let jwt = require('jsonwebtoken')
 
 module.exports = function (express) {
   let router = express.Router()
   router.route('/') // /api/user/
-  .get(function (req, res, next) {
+  .get((req, res, next) => {
     User.find({}, '-password', function(err, users) {
       if (err) {
       	res.status(400).json({'error':err})
@@ -16,7 +16,7 @@ module.exports = function (express) {
   })
   // /api/user/{id}
   router.route('/:_id')
-  .get(function (req, res, next) {
+  .get((req, res, next) => {
     if (req.user) {
       console.log('authenticated request')
     }
@@ -28,6 +28,33 @@ module.exports = function (express) {
         res.status(200).json(user)
       }     
     });
+  })
+  
+  router.route('/:_id/followers')
+  .get((req, res, next) => {
+    User.findOne({_id: req.params._id}, 'followers')
+    .populate('followers', '-password')
+    .exec(function (err, user) {
+      if (err) {
+      	res.status(400).json({'error':err})
+      } else {
+        console.info('Found the user')
+        res.status(200).json(user)
+      }     
+    })
+  })
+  router.route('/:_id/following')
+  .get((req, res, next) => {
+    User.findOne({_id: req.params._id}, 'following')
+    .populate('following', '-password')
+    .exec(function (err, user) {
+      if (err) {
+      	res.status(400).json({'error':err})
+      } else {
+        console.info('Found the user')
+        res.status(200).json(user)
+      }     
+    })
   })
   
   // router.route('/:_id/questions')
