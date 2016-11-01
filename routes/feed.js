4a2830e4e9195d7b8c1fd7998913c25b668cb46d
Nathan Bland <nathan.bland@gmail.com>
2016-11-01 15:32:49 -0600

    alpha testing api endpoint for feed

--- routes/feed.js
+++ routes/feed.js
@@ -12,7 +12,7 @@ module.exports = function (express) {
       if (!req.user) { return res.status(401).json({ error: 'Unauthorized' })}
       console.log(`[garden] GET /api/feed:`, req.user.username)
       User.findOne({_id: req.user._id}, 'following')
-      .populate('following', 'username posts', null
+      .populate('following', 'username posts', null,
         { sort: { 'updatedAt': -1 } })
       // .sort({updatedAt: 1})
       .lean()
