7725e57ddb7f5bab68a8b55becc3e58d48892c57
Nathan Bland <nathan.bland@gmail.com>
2016-11-01 16:25:42 -0600

    Updated to add  ability to update your own posts

--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,27 @@ let jwt = require('jsonwebtoken')
 module.exports = function (express) {
   let router = express.Router()
   
+  router.route('/:id') 
+    // requires user. Adds post to req.user
+    .put((req, res, next) => {
+      console.log(`[garden] PUT /api/post`, req.body)
+      if (!req.user) { return res.status(401).json({ error: 'Unauthorized' })}
+      User.findOneAndUpdate({_id: req.user._id, 'posts._id': req.params.id}, {
+        'posts.$.content': req.body.content,
+        'posts.$.contentType': req.body.contentType
+      }, '-password')
+      .exec(function (err, user) {
+        if (err) { return res.status(400).json({'error':err})
+        } else {
+          user.save(function (err, user) {
+            if (err) { return res.status(400).json({'error':err}) }
+            console.log('Success!');
+            res.status(201).json(user)
+          })
+        }     
+      })
+    })
+  
   router.route('/') 
     // requires user. Adds post to req.user
     .post((req, res, next) => {
